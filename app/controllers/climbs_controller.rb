8d8e48d422641935ab22b5690f820bcd7608612e
Veronica HW <veronica.wajda@gmail.com>
2020-01-10 19:14:47 -0500

    FEAT added geocoder gem

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,3 +1,16 @@
 class ClimbsController < ApplicationController
+  def index
+    @climbs = Climb.where.not(latitude: nil, longitude: nil)
 
+    @markers = @climbs.map do |climb|
+      {
+        lat: climb.latitude,
+        lng: climb.longitude,
+        #infoWindow: render_to_string(partial: "card_window_popup", locals: { climb: climb })
+      }
+    end
+  end
+
+  def show
+  end
 end
