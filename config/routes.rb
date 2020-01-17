ecabe8e2eb1665a0ec41df16649cbec0c2422250
Veronica HW <veronica.wajda@gmail.com>
2020-01-16 17:31:58 -0700

    FEAT: can create new trip report and link it to climb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'trip_reports/index'
-  get 'trip_reports/new'
+  # get 'trip_reports/index'
+  # get 'trip_reports/new'
   devise_for :users
   root to: 'climbs#index'
 
-  resources :trip_reports, only: [:index, :show, :edit, :update, :destroy]
-  resources :climbs, only: [:show] do
-    resources :trip_reports, only: [:new, :create]
-  end
+  resources :trip_reports, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :climbs, only: [:show]
 end
