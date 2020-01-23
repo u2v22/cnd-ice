867be950badd47ce9d1b777b93b29e7eaa4dcea7
Veronica HW <veronica.wajda@gmail.com>
2020-01-23 10:51:32 -0700

    FEAT: updated favorites formatting, model validations, TR default status

--- app/models/trip_report.rb
+++ app/models/trip_report.rb
@@ -4,4 +4,15 @@ class TripReport < ApplicationRecord
   belongs_to :user
 
   STATUS = ['IN', 'not IN', 'Sketchy', 'Status Unknown']
+
+  validates :date, :status, presence: true
+  validates :content, length: { minimum: 50, maximum: 1000 }
+
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if date > Date.today
+      errors.add(:date, "Can't create a TR for a future date")
+    end
+  end
 end
